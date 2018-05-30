988140313a068511d45deab8b5fb9ead00b3d969
Steve Bogucki <sbogucki@mail.usf.edu>
2018-05-30 12:49:45 -0700

    added content to experience component

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Button from '@material-ui/core/Button';
+import Freelance from './freelance';
+import Manager from './manager';
+import Analyst from './analyst';
+import Policy from './policy';
+import Secretarial from './secretarial';
 import '../../App.css';
 
 export default class Experience extends Component {
+	constructor(){
+		super();
+		this.state = {
+			experience: ""
+		}
+	}
+
+	onFreelance(){
+		this.setState({
+			experience: "freelance"
+		})
+	}
+
+	onManager(){
+		this.setState({
+			experience: "manager"
+		})
+	}
+
+	onAnalyst(){
+		this.setState({
+			experience: "analyst"
+		})
+	}
+
+	onPolicy(){
+		this.setState({
+			experience: "policy"
+		})
+	}
+
+	onSecretarial(){
+		this.setState({
+			experience: "secretarial"
+		})
+	}
+
 	render() {
 		return (
 			<Container>
 				<Row className="container">
-				<Button variant="outlined" color="primary">Developer</Button>
-				<Button variant="outlined" color="primary">Manager</Button>
-				<Button variant="outlined" color="primary">Data Analyst</Button>
-				<Button variant="outlined" color="primary">Policy Analyst</Button>
-				<Button variant="outlined" color="primary">Management Assistant</Button>				
+				<Button variant="outlined" color="primary" onClick={()=>this.onFreelance()}>Developer</Button>
+				<Button variant="outlined" color="primary" onClick={()=>this.onManager()}>Manager</Button>
+				<Button variant="outlined" color="primary" onClick={()=>this.onAnalyst()}>Data Analyst</Button>
+				<Button variant="outlined" color="primary" onClick={()=>this.onPolicy()}>Policy Analyst</Button>
+				<Button variant="outlined" color="primary" onClick={()=>this.onSecretarial()}>Management Assistant</Button>				
+				</Row>
+				<Row className="container">
+				{(() => {
+        switch (this.state.experience) {
+          case "freelance":   return <Freelance />;
+          case "manager": return <Manager />;
+		  case "analyst":  return <Analyst />;
+		  case "policy": return <Policy />
+		  case "secretarial": return <Secretarial />
+          default:      return;
+        }
+      })()}
 				</Row>
 			</Container>
 		);
